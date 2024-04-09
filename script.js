8cfda091f6b3ddba2701dfe3a8ff1f8febeff0eb
Juan S Silva <juanse909@hotmail.com>
2024-04-09 08:39:47 -0500

    taller-clase-21

---
+++ script.js
@@ -0,0 +1,91 @@
+const render = () => {
+    const digimones = [
+        {
+          name: "Koromon",
+          img: "https://digimon.shadowsmith.com/img/koromon.jpg",
+          level: "In Training"
+        },
+        {
+          name: "Tsunomon",
+          img: "https://digimon.shadowsmith.com/img/tsunomon.jpg",
+          level: "In Training"
+        },
+        {
+          name: "Yokomon",
+          img: "https://digimon.shadowsmith.com/img/yokomon.jpg",
+          level: "In Training"
+        },
+        {
+          name: "Motimon",
+          img: "https://digimon.shadowsmith.com/img/motimon.jpg",
+          level: "In Training"
+        },
+        {
+          name: "Patamon",
+          img: "https://digimon.shadowsmith.com/img/patamon.jpg",
+          level: "Rookie"
+        },
+        {
+          name: "Kuwagamon",
+          img: "https://digimon.shadowsmith.com/img/kuwagamon.jpg",
+          level: "Champion"
+        },
+        {
+          name: "Greymon",
+          img: "https://digimon.shadowsmith.com/img/greymon.jpg",
+          level: "Champion"
+        },
+        {
+          name: "Tanemon",
+          img: "https://digimon.shadowsmith.com/img/tanemon.jpg",
+          level: "In Training"
+        }
+      ];
+      
+      const digimonList = (digimones) => {
+        let digimonInTraining = [];
+        let digimonRookie = [];
+        let digimonChampion = [];
+        
+        for (digimon of digimones) {
+          if (digimon.level === "In Training") {
+            digimonInTraining.push(digimon);
+          } else if (digimon.level === "Rookie") {
+            digimonRookie.push(digimon);
+          } else if (digimon.level === "Champion") {
+            digimonChampion.push(digimon);
+          }
+    
+        }
+    
+        return {
+            inTraining: digimonInTraining.length,
+            rookie: digimonRookie.length,
+            champion: digimonChampion.length,
+            };
+        }
+    
+        // const digimonReturn = digimonList(digimones);
+        // console.log(digimonReturn.inTraining);
+        // console.log(digimonReturn.rookie);
+        // console.log(digimonReturn.champion);
+    
+        console.log(digimonList(digimones));
+    
+        const container = document.querySelector('.container');
+    
+        const inTraining = document.createElement('p');
+        inTraining.innerHTML = `Existen ${digimonList(digimones).inTraining} digimones con nivel In Training.`;
+    
+        const rookie = document.createElement('p');
+        rookie.innerHTML = `Existen ${digimonList(digimones).rookie} digimones con nivel Champion.`;
+    
+        const champion = document.createElement('p');
+        champion.innerHTML = `Existen ${digimonList(digimones).champion} digimones con nivel Rookie.`;
+    
+        container.appendChild(inTraining);
+        container.appendChild(rookie);
+        container.appendChild(champion);
+};    
+    document.addEventListener("DOMContentLoaded", render);
+
